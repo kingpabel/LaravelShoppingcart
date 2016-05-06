14c97d82ade13d351ea3712b685449c244a9d9b1
kingpabel <ippabel@gmail.com>
2016-05-07 04:01:08 +0600

    few property are used

--- src/CartCollection.php
+++ src/CartCollection.php
@@ -4,4 +4,11 @@ use Illuminate\Support\Collection;
 
 class CartCollection extends Collection {
 
+    public $discount = 0.00;
+
+    public $custom_discount = 0.00;
+
+    public $total = 0.00;
+
+    public $subtotal = 0.00;
 }
